ba8e50667b9a076cd5feffb46ec0dae79a056421
Ben Terry <beterry12@gmail.com>
2019-11-10 17:34:06 -0500

    Created Short Term component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,15 +9,26 @@ import Arrows from '../Arrows/Arrows.component'
 import LifetimeValue from '../LifetimeValue/LifetimeValue.component'
 import Referrals from '../Referrals/Referrals.component'
 import TotalLifetime from '../TotalLifetime/TotalLifetime.component'
+import ShortTerm from '../ShortTerm/ShortTerm.component'
 
 //import styles
 import styles from './App.module.css'
 
 function App() {
+
+  //declare state
+  //variables that the user is able to update
   const [aro, setAro] = useState(573)
   const [visits, setVisits] = useState(2)
   const [years, setYears] = useState(5)
   const [referrals, setReferrals] = useState(2)
+  const [quantity, setQuantity] = useState(10000)
+  const [redemptionRate, setRedemptionRate] = useState(0.005)
+
+  //cost of mailer
+  const cost = 3500
+
+  //define functions to update state
   const changeAro = (newValue) => {
     console.log(newValue)
     setAro(newValue)
@@ -34,6 +45,15 @@ function App() {
     console.log(newValue)
     setReferrals(newValue)
   }
+  const changeQuantity = (newValue) => {
+    console.log(newValue)
+    setQuantity(newValue)
+  }
+  const changeRedemptionRate = (newValue) => {
+    console.log(newValue)
+    setRedemptionRate(newValue)
+  }
+
   return (
     <Layout>
       <Header />
@@ -60,6 +80,14 @@ function App() {
         visits={visits}
         years={years}
         referrals={referrals}
+      />
+      <ShortTerm
+      aro={aro}
+        quantity={quantity}
+        redemptionRate={redemptionRate}
+        onChangeQuantity={changeQuantity}
+        onChangeRedemptionRate={changeRedemptionRate}
+        cost={cost}
       />
     </Layout>
   )
---
+++ src/components/ShortTerm/ShortTerm.component.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+
+//import styles
+
+//import components
+import Step from '../Step/Step.component'
+import LeftCopy from '../LeftCopy/LeftCopy.component'
+import Calculation from '../Calculation/Calculation.component'
+import Illustration from '../Illustration/Illustration.component'
+import Slider from '../Slider/Slider.component'
+import Total from '../Total/Total.component'
+
+//import images
+import tempImage from '../../images/instructions/car-red.png'
+
+export default ({aro, quantity, redemptionRate, cost, onChangeRedemptionRate, onChangeQuantity}) => (
+    <Step>
+        <LeftCopy
+            title={'STEP 1 OF 3'}
+            color={'orange'}
+        >
+            <p>Now, using the Average Repair Order you specified in Step 1 in Section 1, we can calculate your short term roi on your direct mail campaign.</p>
+        </LeftCopy>
+        <Calculation
+            color={'orange'}
+            title={'SHORT TERM DIRECT MAIL ROI'}
+        >
+            <Slider
+                title={'Quantity Mailed'}
+                total={quantity}
+                options={[1000, 5000, 10000, 15000, 20000]}
+                onChange={onChangeQuantity}
+            />
+            <Slider
+                title={'Redemption Rate %'}
+                total={redemptionRate}
+                options={[0.005, 0.01, 0.02, 0.03, 0.05]}
+                onChange={onChangeRedemptionRate}
+            />
+            <Slider
+                title={'Increased Car Count'}
+                total={quantity * redemptionRate}
+            />
+            <Slider
+                title={'Average Repair Cost'}
+                total={aro}
+            />
+            <Slider
+                title={'Gross Revenue'}
+                total={aro * (quantity * redemptionRate)}
+            />
+            <Slider
+                title={'Less Estimated Cost of Mailer'}
+                total={cost}
+            />
+            <Slider
+                title={'Net Revenue'}
+                total={(aro * quantity * redemptionRate) - cost}
+            />
+            <Total
+                equation={`$${(aro * quantity * redemptionRate) - cost} - $${cost} =`}
+                total={((aro * quantity * redemptionRate) - cost) / cost}
+            />
+        </Calculation>
+        <Illustration
+            src={tempImage}
+            alt={'This is a temporary image'}
+        />
+    </Step>
+)
