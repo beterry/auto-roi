a20b72a8cc9bc143f9a1c3e01f5352af97421e12
Ben Terry <beterry12@gmail.com>
2019-11-19 15:11:47 -0500

    add icon to toggle theme button

--- src/components/Header/Header.component.jsx
+++ src/components/Header/Header.component.jsx
@@ -6,16 +6,31 @@ import styles from './Header.module.css'
 //import images
 import lightLogo from '../../images/logo/MS_FullColor_2019.svg'
 import darkLogo from '../../images/logo/MS_White_2019.svg'
+import sun from '../../images/toggle/sun.svg'
+import moon from '../../images/toggle/moon.svg'
 
-export default ({changeTheme, theme}) => (
-    <div className={styles.header}>
-        <img src={theme ? lightLogo : darkLogo} alt={'Mail Shark logo'}/>
-        <div className={styles.matter}>
-            <button
-                className={styles.toggleTheme}
-                onClick={() => changeTheme()}
-            >Toggle Theme</button>
-            <p className={theme ? 'light' : 'dark'}>999-999-9999</p>
+export default ({changeTheme, theme}) => {
+    const buttonStyles = {
+        light: {
+            background: '#002A42'
+        },
+        dark: {
+            background: '#FF8C00'
+        }
+    }
+    return(
+        <div className={styles.header}>
+            <img src={theme ? lightLogo : darkLogo} alt={'Mail Shark logo'}/>
+            <div className={styles.matter}>
+                <button
+                    className={styles.toggleTheme}
+                    style={theme ? buttonStyles.light : buttonStyles.dark}
+                    onClick={() => changeTheme()}
+                >
+                    <img src={theme ? moon : sun} />
+                </button>
+                <p className={theme ? 'light' : 'dark'}>999-999-9999</p>
+            </div>
         </div>
-    </div>
-)
+    )
+}
