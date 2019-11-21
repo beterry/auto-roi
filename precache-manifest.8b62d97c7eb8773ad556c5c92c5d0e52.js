1f4bd54abf818e6661c7c15fd75df77f1ab6b8e8
Ben Terry <beterry12@gmail.com>
2019-11-21 15:19:33 -0500

    Updates

---
+++ precache-manifest.8b62d97c7eb8773ad556c5c92c5d0e52.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9408a48d3adc0a569203c6bb8f2a2652",
+    "url": "/auto-roi/index.html"
+  },
+  {
+    "revision": "0116c62ebda4524cd5a8",
+    "url": "/auto-roi/static/css/main.292059b3.chunk.css"
+  },
+  {
+    "revision": "b88c8ba9c192c5c52a65",
+    "url": "/auto-roi/static/js/2.c15960c5.chunk.js"
+  },
+  {
+    "revision": "0116c62ebda4524cd5a8",
+    "url": "/auto-roi/static/js/main.49b72144.chunk.js"
+  },
+  {
+    "revision": "2c3f1b1b827b079c3723",
+    "url": "/auto-roi/static/js/runtime-main.ad08f81d.js"
+  },
+  {
+    "revision": "e24f72c91b383690835ec3680bce4eb2",
+    "url": "/auto-roi/static/media/MS_FullColor_2019.e24f72c9.svg"
+  },
+  {
+    "revision": "386d34528afffab12b380fdcdd99ce67",
+    "url": "/auto-roi/static/media/MS_White_2019.386d3452.svg"
+  },
+  {
+    "revision": "19ebc48913440a67e91495d70a4fe7e3",
+    "url": "/auto-roi/static/media/arrows.19ebc489.svg"
+  },
+  {
+    "revision": "3ac19a70e220ac1fdd84e7673b438185",
+    "url": "/auto-roi/static/media/car-blue.3ac19a70.svg"
+  },
+  {
+    "revision": "a799c6e24c38a406a9ff027b5a5017f5",
+    "url": "/auto-roi/static/media/car-red.a799c6e2.svg"
+  },
+  {
+    "revision": "54b8bce3d8871483341cab782eb989b8",
+    "url": "/auto-roi/static/media/favicon-white.54b8bce3.svg"
+  },
+  {
+    "revision": "0f05db441ee5ff3cf9bada657ae781f5",
+    "url": "/auto-roi/static/media/money-large-whole.0f05db44.svg"
+  },
+  {
+    "revision": "4299f2307e4bd82e4e0f97d725c5749a",
+    "url": "/auto-roi/static/media/money-medium-whole.4299f230.svg"
+  },
+  {
+    "revision": "08cb49949581cc42c049efd9bb098689",
+    "url": "/auto-roi/static/media/money-pile.08cb4994.svg"
+  },
+  {
+    "revision": "3dc8329482f787efa6c4950d592b7f4a",
+    "url": "/auto-roi/static/media/money-small-whole.3dc83294.svg"
+  },
+  {
+    "revision": "0d42106b7fbcbb28b4ce963336194417",
+    "url": "/auto-roi/static/media/moon.0d42106b.svg"
+  },
+  {
+    "revision": "485af7260f51871af8f15bd3e0272f8e",
+    "url": "/auto-roi/static/media/phones-large-whole.485af726.svg"
+  },
+  {
+    "revision": "5b216fb582ebc23ed65a3e51c3a8774f",
+    "url": "/auto-roi/static/media/phones-medium-whole.5b216fb5.svg"
+  },
+  {
+    "revision": "53b1ca4c938db51753d7994464e82e42",
+    "url": "/auto-roi/static/media/phones-small-whole.53b1ca4c.svg"
+  },
+  {
+    "revision": "9a4c0c6053094f315d9db29de6866134",
+    "url": "/auto-roi/static/media/pig-large.9a4c0c60.svg"
+  },
+  {
+    "revision": "1ef7a51d4a634b216b7a5a46b9323bb5",
+    "url": "/auto-roi/static/media/pig-medium.1ef7a51d.svg"
+  },
+  {
+    "revision": "69def1d790b040b63d9ae238bc8f2f7e",
+    "url": "/auto-roi/static/media/pig-small.69def1d7.svg"
+  },
+  {
+    "revision": "a916ef00a9c1e92d6f2adc4f7532975c",
+    "url": "/auto-roi/static/media/sun.a916ef00.svg"
+  },
+  {
+    "revision": "49aff6a088cd7c49c6314f79d7cdb86f",
+    "url": "/auto-roi/static/media/testimonials-large-whole.49aff6a0.svg"
+  },
+  {
+    "revision": "1848ec2c3dca224a5ea6db0283efc607",
+    "url": "/auto-roi/static/media/testimonials-medium-whole.1848ec2c.svg"
+  },
+  {
+    "revision": "065b091ab58dddacfe7b5c977642a481",
+    "url": "/auto-roi/static/media/testimonials-small-whole.065b091a.svg"
+  },
+  {
+    "revision": "c45ced19c3920bbcd8d1a47301868e21",
+    "url": "/auto-roi/static/media/tip-triangle.c45ced19.svg"
+  },
+  {
+    "revision": "1c206eeb7e229a30aaeb121f9532252e",
+    "url": "/auto-roi/static/media/tooltip-white.1c206eeb.svg"
+  },
+  {
+    "revision": "0bcc46e7cdfa7e5d94a8d9a1bc18111d",
+    "url": "/auto-roi/static/media/tooltip.0bcc46e7.svg"
+  },
+  {
+    "revision": "9a71b68dcd3897317fe952b414093a96",
+    "url": "/auto-roi/static/media/total-lifetime.9a71b68d.svg"
+  }
+]);
